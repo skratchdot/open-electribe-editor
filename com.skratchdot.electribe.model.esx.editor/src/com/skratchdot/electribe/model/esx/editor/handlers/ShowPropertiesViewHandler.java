4944b253178aac5cc86ad68e80d1d1c94c6676b5
skratchdot <git@skratchdot.com>
2010-03-07 18:42:03 +0000

    Adding "Show Properties View" command
    git-svn-id: http://open-electribe-editor.googlecode.com/svn/trunk@119 8452c704-95ec-11de-bf1c-4b96ac7417a7

---
+++ com.skratchdot.electribe.model.esx.editor/src/com/skratchdot/electribe/model/esx/editor/handlers/ShowPropertiesViewHandler.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2009-2010 SKRATCHDOT.COM
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    JEFF |:at:| SKRATCHDOT |:dot:| COM
+ *
+ * $Id$
+ */
+package com.skratchdot.electribe.model.esx.editor.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+public class ShowPropertiesViewHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getActivePage().showView(IPageLayout.ID_PROP_SHEET);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
